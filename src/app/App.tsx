d32072dc3af0330c23f716ac53836b508b348378
vseravno <vseravno2019@gmail.com>
2023-02-21 22:35:39 +0700

    feat(ui-comp): added app-link & navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import './styles/index.scss'
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from 'widgets/Navbar'
 
 const App = () => {
     const {theme,toggleTheme} = useTheme();
 
     return(
         <div className={classNames('app',{},[theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     )
 }
