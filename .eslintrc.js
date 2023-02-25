c5c1d449e05694c2d687a7a703aa089e08beab02
vseravno <vseravno2019@gmail.com>
2023-02-25 18:53:40 +0700

    feat(babel): added stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'airbnb',
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -18,6 +19,7 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
+        'i18next',
     ],
     rules: {
         // отступы
@@ -50,6 +52,8 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         // запрещает андерскор
         'no-underscore-dangle': 'off',
+        // ругается если забыли локализацию
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,
